7fba685fccbafd1ab163a1c5fc2612ed14aa77c3
Veli Bacık <velibacik60@gmail.com>
2019-03-30 03:03:53 +0300

    stack wrap todo

--- lib/screen/login/login.dart
+++ lib/screen/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:highschool/component/box/sized.dart';
+import 'package:highschool/util/const/images.dart';
 import 'package:highschool/util/styles/color.dart';
 import 'package:highschool/util/styles/text.dart';
 
@@ -9,80 +10,87 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String title = "Eğitim Yönetim Sistemi";
+  String title = "School Education System";
 
   @override
   Widget build(BuildContext context) {
+    //TODO WRAP STACK !! look https://medium.com/flutter-community/a-deep-dive-into-stack-in-flutter-3264619b3a77
     return Scaffold(
-      body: Container(
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Container(
-                  color: Colors.red,
-                  child: Center(
-                    child: Text(
-                      this.title,
-                      style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.white,
-                          fontFamily: "PermanentMarker"),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              flex: 3,
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Image.asset(
+                      LOGIN_highSchool,
+                      fit: BoxFit.fill,
                     ),
                   ),
-                ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                    color: LOGIN_backgroundColor,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 30),
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 20,
-                          ),
-                          TextField(
-                            textAlign: TextAlign.left,
-                            decoration: inputDecoration(
-                                "Sicil Kimlik veya TC. No Giriniz",
-                                Icons.account_circle),
-                            style: inputTextStyle,
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          TextField(
-                            textAlign: TextAlign.left,
-                            decoration: inputDecoration(
-                                "Şifrenizi Giriniz", Icons.account_circle),
-                            style: inputTextStyle,
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          CustomSizedBox(
-                            child: RaisedButton(
-                              onPressed: () => {},
-                              color: LOGIN_submitButtonColor,
-                              child: Container(
-                                child: Text(
-                                  "Giriş Yap",
-                                  textAlign: TextAlign.center,
-                                  style: buttonTextStyle,
-                                ),
-                              ),
-                            ),
-                            height: 50,
-                          )
-                        ],
+                  Container(
+                    color: LOGIN_imageBackgroundColor,
+                    child: Center(
+                      child: Text(
+                        this.title,
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontFamily: "PermanentMarker"),
                       ),
-                    )),
-              )
-            ],
-          )),
+                    ),
+                  )
+                ],
+              )),
+          Expanded(
+            flex: 4,
+            child: Container(
+                color: LOGIN_backgroundColor,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        textAlign: TextAlign.left,
+                        decoration: inputDecoration(
+                            "School Number", Icons.supervised_user_circle),
+                        style: inputTextStyle,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        textAlign: TextAlign.left,
+                        decoration: inputDecoration(
+                            "School Password", Icons.lock_open),
+                        style: inputTextStyle,
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      CustomSizedBox(
+                        child: RaisedButton(
+                          onPressed: () => {},
+                          color: LOGIN_submitButtonColor,
+                          child: Container(
+                            child: Text(
+                              "Giriş Yap",
+                              textAlign: TextAlign.center,
+                              style: buttonTextStyle,
+                            ),
+                          ),
+                        ),
+                        height: 50,
+                      )
+                    ],
+                  ),
+                )),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/util/styles/color.dart
+++ lib/util/styles/color.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 const LOGIN_backgroundColor = Color.fromRGBO(247, 247, 247, 1);
 const LOGIN_submitButtonColor = Color.fromRGBO(2, 75, 128, 1.0);
+
+
+const LOGIN_imageBackgroundColor = Color.fromRGBO(203, 35, 33, 0.76);
